29bde3638eeedb3c4898fdabf0a4bc7191cc50b9
Sinyuk <80998062@qq.com>
2016-10-22 01:28:12 +0800

    Update

---
+++ myutils/src/main/java/com/sinyuk/myutils/resource/AssetsUtils.java
@@ -0,0 +1,52 @@
+package com.sinyuk.myutils.resource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import android.content.Context;
+
+/**
+ * AssetsUtils
+ * 
+ * @author cundong 2014-9-5
+ */
+public class AssetsUtils {
+
+	private static final String ENCODING = "utf-8";
+
+	public static String loadText(Context context, String assetFielPath){
+		InputStream is = null;
+		try {
+			is = context.getResources().getAssets().open(assetFielPath);
+			return convertStreamToString(is);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+		return null;
+	}
+	
+	public static String convertStreamToString(InputStream is)
+			throws IOException {
+		Writer writer = new StringWriter();
+
+		char[] buffer = new char[2048];
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(is,
+					ENCODING));
+			int n;
+			while ((n = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, n);
+			}
+		} finally {
+			is.close();
+		}
+		return writer.toString();
+	}
+}
